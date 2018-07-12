9546a369dbcf4be0c2af296523fc91450dd98d20
Kevin Huang <yellow.khaos@gmail.com>
2018-07-12 05:06:54 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
     get "/" => "application#homepage"
+
+    post "/" => "application#index"
   # Example resource route with options:
   #   resources :products do
   #     member do
